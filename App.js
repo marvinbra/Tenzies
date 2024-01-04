3fb3689a2e7fc01b0dec386c8a21408d38cb8d53
marvinbra <marvinbopp37@gmail.com>
2024-01-04 19:43:56 +0100

    state in App.js
    react

---
+++ App.js
@@ -0,0 +1,8 @@
+import React from "react"
+import Die from "./Die"
+import {nanoid} from "nanoid"
+
+export default function App() {
+
+    const [dice, setDice] = React.useState(allNewDice())
+    const [tenzies, setTenzies] = React.useState(false)
